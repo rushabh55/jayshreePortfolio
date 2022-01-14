c6dbe4933908c3485b525718c2702d35acded76f
Rushabh Gosar <admin@rushg.me>
2022-01-13 23:00:22 -0800

    LFSrevert

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,32 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:69523ce47fc7be813c1183422779ad247fbe0faa5a2fdf298ff591a03155bae5
-size 715
+/*
+	Photon by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1141px',  '1680px' ],
+			large:    [ '981px',   '1140px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '321px',   '480px'  ],
+			xxsmall:  [ null,      '320px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Scrolly.
+		$('.scrolly').scrolly();
+
+})(jQuery);
